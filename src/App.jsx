90b85d246ccfddd8f58f6491284fca14b1261092
HP <ayebel007@gmail.com>
2025-03-12 21:57:10 +0530

    env variables added

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,27 @@
 import { useEffect, useState } from 'react';
 import axios from 'axios';
 import './App.css';
-const apiUrl = import.meta.env.VITE_Access_key;
 
 function App() {
-  const [fromCurrency, setFromCurrency] = useState("INR")
-  const [toCurrency, setToCurrency] = useState("USDS")
-  const [amount, setAmount] = useState(0)
-  const [livePrice, setLivePrice] = useState(null)
-
+  const [fromCurrency, setFromCurrency] = useState("USD");
+  const [toCurrency, setToCurrency] = useState("INR");
+  const [amount, setAmount] = useState(0);
+  const [livePrice, setLivePrice] = useState(null);
+ const  apiUrl = import.meta.env.VITE_Access_key;
   function getRate() {
-    axios.get(`https://api.metalpriceapi.com/v1/convert?api_key=${apiUrl}&from=${fromCurrency}&to=${toCurrency}&amount=${amount}&date=2025-03-01`)
+    axios.get(`https://api.metalpriceapi.com/v1/convert?api_key=${apiUrl}&from=${fromCurrency}&to=${toCurrency}&amount=${amount}`)
       .then((response) => {
         console.log(response.data.result);
         setLivePrice(response.data.result);
       })
       .catch((error) => {
-        console.log(error)
+        console.log(error);
       });
   }
 
   useEffect(() => {
-     getRate()
-    },[amount, fromCurrency, toCurrency])
+   getRate()
+  }, [amount, fromCurrency, toCurrency]);
 
   return (
     <>
@@ -35,7 +34,7 @@ function App() {
               <option value="INR">INR</option>
               <option value="EUR">EUR</option>
             </select>
-            <label> to </label>
+            <span> to </span>
             <select value={toCurrency} onChange={(e) => setToCurrency(e.target.value)}>
               <option value="USD">USD</option>
               <option value="INR">INR</option>
@@ -45,7 +44,7 @@ function App() {
           <div>
             <input type="number" placeholder="Enter amount" onChange={(e) => setAmount(e.target.value)} />
             <label>Converted Amount: </label>
-            <p>{livePrice}</p>
+            <p>{livePrice !== null ? livePrice : "Enter amount to convert"}</p>
           </div>
         </div>
       </div>
